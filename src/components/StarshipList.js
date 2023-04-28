d2ba050ad92d26db2175a72831f583a790902792
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-28 16:48:53 +0300

    list  starship

---
+++ src/components/StarshipList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CardList from "./Card/CardList";
+
+function StarshipList() {
+  return (
+    <div>
+      <CardList />
+    </div>
+  );
+}
+
+export default StarshipList;
