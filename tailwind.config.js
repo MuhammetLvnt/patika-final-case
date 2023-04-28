181ec81a785dc24cff40d6765ab2cc4475f7f89e
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-28 16:56:16 +0300

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    transitionDuration: {
+      1300: "1300ms",
+    },
+    fontFamily: {
+      bruno: ["Bruno Ace SC"],
+    },
   },
   plugins: [],
 };
