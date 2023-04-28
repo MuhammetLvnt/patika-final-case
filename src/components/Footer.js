013e765846d0db80a7b378af3f0b206719e24432
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-28 16:56:30 +0300

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { FaRegCopyright } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-600 rounded-md flex flex-col justify-center items-center p-8 font-bruno mt-10">
+      <div className="flex justify-center items-center space-x-3 text-[#00FF00]">
+        <FaRegCopyright />
+        <p className="text-lg font-light leading-relaxed tracking-wide">
+          Muhammet Levent
+        </p>
+      </div>
+      <div>
+        <p className="text-lg font-light leading-relaxed tracking-wide">
+          fmss bilişim front-end practicum
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
